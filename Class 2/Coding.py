c709321abe43b475defea2ef31b52073c3ad4623
Inaaya-2012 <bhardwajdivya81.bd@gmail.com>
2025-06-30 12:03:13 +0530

    Class 2

---
+++ Class 2/Coding.py
@@ -0,0 +1,16 @@
+# print statement
+
+print("Welcome to the world of coding")
+
+print(10)
+
+print("a = ",10)
+
+print("Name\tage\tphone\nInaaya\t13\t9873232421")
+
+print("I am learning coding",end = ",")
+print("it is very easy")
+
+n1 = 10
+print("n1 = ",n1+10)
+      
---
+++ Class 2/Variables.py
@@ -0,0 +1,15 @@
+# understanding variables
+
+name = "Inaaya"
+
+age = 13
+
+print(name)
+print(age)
+
+print("My name is ",name)
+print("My age is ",age)
+
+num1 = input("Enter your name")
+
+print("Welcome to world of coding",num1,"You will have a lot of fun")
