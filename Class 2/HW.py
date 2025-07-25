4842e07b064257803ade9594ad663569c70e7925
Inaaya-2012 <bhardwajdivya81.bd@gmail.com>
2025-07-25 20:53:21 +0530

    Class 2 hw

---
+++ Class 2/HW.py
@@ -0,0 +1,14 @@
+db = 14
+sb = 18
+pb = 25
+nb = 9
+print(db)
+print(sb)
+print(pb)
+print(nb)
+
+print("My mother's birthdate is",db)
+print("My sister's birthdate is",sb)
+print("My father's birthdate is",pb)
+print("My birthdate is",nb)
+
