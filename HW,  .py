cb88e6cfa8186b4f2d3725be4032fc5e07a50a26
Inaaya-2012 <bhardwajdivya81.bd@gmail.com>
2025-10-06 20:54:22 +0530

    Add files via upload

---
+++ HW,  .py
@@ -0,0 +1,17 @@
+# Function to calculate product of elements in a tuple
+def calculate_product(tup):
+    product = 1
+    for num in tup:
+        product *= num
+    return product
+
+# Given tuples
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+
+# Calculate and print the product
+product1 = calculate_product(tup1)
+product2 = calculate_product(tup2)
+
+print("Product of tup1:", product1)
+print("Product of tup2:", product2)
