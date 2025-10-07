9219071c84a95d6408048b0f26b104209317514c
Inaaya-2012 <bhardwajdivya81.bd@gmail.com>
2025-10-07 11:42:56 +0530

    Add files via upload

---
+++ HW,,,,,,,,,,,,,,, .py
@@ -0,0 +1,5 @@
+temperature = float(input("Enter the temperature in Celsius: "))
+if temperature >= 25:
+    print("It's warm enough to wear light clothes.")
+else:
+    print("It's better to wear something warmer.")
